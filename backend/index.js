c4c27ab0e034bb9069f1c21286257da181fc3ac7
jhansiaderu <aderujhansi1544@gmail.com>
2021-09-14 10:21:45 +0530

    video 44

--- backend/index.js
+++ backend/index.js
@@ -9,6 +9,11 @@ app.get('/', (req, res) => {
   res.send('Hello Jhansi!')
 })
 
+
+// Available Routes
+app.use('/api/auth', require('./routes/auth'))
+//app.use('/api/notes', require('./routes/notes'))
+
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
 })
