d44cc2bd7f8be8a8ecea87fad6442c07f4e069ba
jhansiaderu <aderujhansi1544@gmail.com>
2021-09-14 11:19:02 +0530

    video 45

--- backend/index.js
+++ backend/index.js
@@ -4,7 +4,7 @@ const express = require('express')
 connectToMongo();
 const app = express()
 const port = 3000
-
+app.use(express.json())
 app.get('/', (req, res) => {
   res.send('Hello Jhansi!')
 })
@@ -12,7 +12,7 @@ app.get('/', (req, res) => {
 
 // Available Routes
 app.use('/api/auth', require('./routes/auth'))
-//app.use('/api/notes', require('./routes/notes'))
+app.use('/api/notes', require('./routes/notes'))
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
