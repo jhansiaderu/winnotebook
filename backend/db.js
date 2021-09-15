b5442800910c04eca2908d8e7ce50462642fd0d5
Aderu Jhansi <89960596+jhansiaderu@users.noreply.github.com>
2021-09-15 13:50:37 +0530

    Update db.js

--- backend/db.js
+++ backend/db.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-const mongoURI ="mongodb+srv://jhansi:<Srinivasulu123>@cluster0.dix0x.mongodb.net/winnotebook?retryWrites=true&w=majority"
+const mongoURI ="mongodb+srv://jhansi:<password>@cluster0.dix0x.mongodb.net/winnotebook?retryWrites=true&w=majority"
 const connectToMongo = ()=>{
     mongoose.connect(mongoURI, ()=>{
         console.log("Connected to Mongo Successfully");
