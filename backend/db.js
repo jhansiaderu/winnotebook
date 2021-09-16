032bf81c499f959290e7dea64512c32353ae4222
jhansiaderu <aderujhansi1544@gmail.com>
2021-09-16 13:20:21 +0530

    video 52

--- backend/db.js
+++ backend/db.js
@@ -1,10 +1,6 @@
 const mongoose = require('mongoose');
 
-<<<<<<< HEAD
 const mongoURI ='mongodb+srv://jhansi:noteapp@cluster0.om5en.mongodb.net/notebook?retryWrites=true&w=majority'
-=======
-const mongoURI ="mongodb+srv://jhansi:<password>@cluster0.dix0x.mongodb.net/winnotebook?retryWrites=true&w=majority"
->>>>>>> b5442800910c04eca2908d8e7ce50462642fd0d5
 const connectToMongo = ()=>{
     mongoose.connect(mongoURI, ()=>{
         console.log("Connected to Mongo Successfully");
--- backend/routes/notes.js
+++ backend/routes/notes.js
@@ -1,9 +1,38 @@
 const express = require('express');
 const router = express.Router();
+var fetchUser = require('../middleware/fetchUser');
+const { body, validationResult } = require('express-validator');
 
-router.get('/fetchallnotes', (req, res)=>{
-    
-    res.json([])
+
+// ROUTE 1: Get All the Notes using: GET "/api/auth/getuser"... Login required
+router.get('/fetchallnotes',fetchUser,async (req, res)=>{
+    const notes = awaitNotes.find({user:req.user.id});
+    res,json(notes)
+    //res.json([notes])
 } )
+// ROUTE 2: add a new Note using: post "/api/auth/addnote"... Login required
+router.post('/addnote', fetchUser, [
+    body('title', 'Enter a valid title').isLength({ min: 3 }),
+    body('description', 'Description must be atleast 5 characters').isLength({ min: 5 }),], async (req, res) => {
+        try {
 
+            const { title, description, tag } = req.body;
+
+            // If there are errors, return Bad request and the errors......
+            const errors = validationResult(req);
+            if (!errors.isEmpty()) {
+                return res.status(400).json({ errors: errors.array() });
+            }
+            const note = new Note({
+                title, description, tag, user: req.user.id
+            })
+            const savedNote = await note.save()
+
+            res.json(savedNote)
+
+        } catch (error) {
+            console.error(error.message);
+            res.status(500).send("Internal Server Error");
+        }
+    })
 module.exports = router
