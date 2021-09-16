981e3f1d0fdc9eb568b7454a6b227537a805d831
jhansiaderu <aderujhansi1544@gmail.com>
2021-09-16 18:05:13 +0530

    video 56

--- src/App.js
+++ src/App.js
@@ -1,11 +1,28 @@
-import logo from './logo.svg';
 import './App.css';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from "react-router-dom";
+import Navbar from './components/Navbar';
+import { Home } from './components/Home';
+import About from './components/About';
 
 function App() {
   return (
-    <div className="App">
-      <h1>this is for testing</h1>
-    </div>
+    <>
+      <Router>
+        <Navbar /> 
+        <Switch>
+          <Route exact path="/">
+            <Home />
+          </Route>
+          <Route exact path="/about">
+            <About/>
+          </Route> 
+        </Switch>
+      </Router>
+    </>
   );
 }
 
