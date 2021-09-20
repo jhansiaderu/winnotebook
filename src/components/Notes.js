bd71572bce73cf1808baa748f43e1f9352c2637c
jhansiaderu <aderujhansi1544@gmail.com>
2021-09-20 10:07:31 +0530

    video 63

--- src/components/Notes.js
+++ src/components/Notes.js
@@ -1,12 +1,15 @@
 import React ,{useContext} from 'react'
 import noteContext from "../context/notes/noteContext"
 import Noteitem from './Noteitem';
-
+import AddNote from './AddNote';
 
  const Notes = () => {
     const context = useContext(noteContext);
-    const {notes, setNotes} = context;
+    const {notes , addNote} = context;
+    
     return (
+        <>
+        <AddNote/>
         <div>
          <div className="row my-3">
          <h2>your notes</h2>
@@ -17,6 +20,7 @@ import Noteitem from './Noteitem';
          </div>
             
         </div>
+        </>
     )
 }
 export default Notes
--- src/context/notes/NoteState.js
+++ src/context/notes/NoteState.js
@@ -56,19 +56,49 @@ const NoteState = (props)=>{
        },
        {
         name:  "jha",
-        _id:"12365387367220",
+        _id:"12365387367225",
         email:"jha343@gmail.com",
         password:"$2a$10$XZdFjgBMT8aEl5u1d0lG2uE0AQi9p21uoKt0ToZpOSqxLgW/zFB/i",
-        title:"my title",
-        description:"please make it fast",
+        title:"title",
+        description:" make it fast",
         date : "2021-09-16T09:57:41.851+00:00",
         __v:0
        }
        
    ]
    const [notes, setNotes] =  useState(notesInitial)
+
+   //Add a note
+
+   const addNote = (title, description, tag)=>{
+    // TODO: API Call
+    console.log("Adding a new note")
+    const note = {
+      "_id": "61322f119553781a8ca8d0e08",
+      "user": "6131dc5e3e4037cd4734a0664",
+      "title": title,
+      "description": description,
+      "tag": tag,
+      "date": "2021-09-03T14:20:09.668Z",
+      "__v": 0
+    };
+    setNotes(notes.concat(note)) 
+  }
+
+   //delete a note
+
+   const deleteNote =() =>{
+       
+   }
+
+
+   //edit a note
+
+   const editNote =() =>{
+       
+   }
     return (
-        <NoteContext.Provider value={{notes , setNotes}}>
+        <NoteContext.Provider value={{notes , addNote ,deleteNote, editNote}}>
             {props.children}
         </NoteContext.Provider>
     )
